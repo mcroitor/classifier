808732e9bb1dcbefe25b6122320d619084a96a36
Mihail Croitor <mcroitor@gmail.com>
2022-10-25 11:48:36 +0300

    tests added

---
+++ tests/test01.php
@@ -0,0 +1,39 @@
+<?php
+
+include __DIR__ . "/../src/mc/classifier.php";
+
+function test(bool $expression, string $passed = "PASS", string $failed = "FAIL"): void {
+    echo $expression ? $passed : $failed;
+    echo PHP_EOL;
+}
+
+function info(string $message, $object = null): void {
+    echo "[info] $message";
+    if($object != null){
+        echo " - " . json_encode($object);
+    }
+    echo PHP_EOL;
+}
+
+$data = [
+    "one",
+    "two",
+    "three",
+    "four"
+];
+$classifier_name = "enumeration";
+
+info("test1: create a classifier");
+info("init data:", $data);
+
+$enumeration = new \mc\classifier($classifier_name, $data);
+
+info("classifier name: ", $enumeration->name());
+
+test($classifier_name === $enumeration->name());
+
+info("total items in classifier: ", $enumeration->count());
+test($enumeration->count() === count($data));
+
+info("item 1 in classifier: ", $enumeration->get(1));
+test($enumeration->get(1) === $data[1]);
---
+++ tests/test02.php
@@ -0,0 +1,36 @@
+<?php
+
+include __DIR__ . "/../src/mc/classifier.php";
+
+function test(bool $expression, string $passed = "PASS", string $failed = "FAIL"): void {
+    echo $expression ? $passed : $failed;
+    echo PHP_EOL;
+}
+
+function info(string $message, $object = null): void {
+    echo "[info] $message";
+    if($object != null){
+        echo " - " . json_encode($object);
+    }
+    echo PHP_EOL;
+}
+
+$classifier_file = __DIR__ . "/countries.json";
+$classifier_name = "countries";
+$classifier_size = 249;
+$key = "MDA";
+$value = "Moldova (the Republic of)";
+
+info("test2: load classifier from file");
+
+$countries = \mc\classifier::load($classifier_file);
+
+info("classifier name: ", $countries->name());
+
+test($classifier_name === $countries->name());
+
+info("total items in classifier: ", $countries->count());
+test($countries->count() === $classifier_size);
+
+info("item {$key} in classifier: ", $countries->get($key));
+test($countries->get($key) === $value);
